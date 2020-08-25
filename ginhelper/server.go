f6325acd45fba2e94dfeddb2558ccc442a17c6a2
zcong1993 <zhangcong1992@gmail.com>
2020-08-25 16:55:18 +0800

    chore: update default server

--- ginhelper/server.go
+++ ginhelper/server.go
@@ -3,18 +3,16 @@ package ginhelper
 import (
 	"github.com/gin-gonic/gin"
 	logKit "github.com/go-kit/kit/log"
-	"github.com/zcong1993/x/log"
 	"github.com/zcong1993/x/log/flag"
 	"gopkg.in/alecthomas/kingpin.v2"
 )
 
 func DefaultServer() (*gin.Engine, logKit.Logger) {
-	var logConfig = log.Config{}
-	flag.AddFlags(kingpin.CommandLine, &logConfig)
+	loggerF := flag.NewFactoryFromFlags(kingpin.CommandLine)
 	kingpin.CommandLine.GetFlag("help").Short('h')
 	kingpin.Parse()
 
-	logger := log.New(&logConfig)
+	logger := loggerF()
 	r := DefaultWithLogger(logger)
 	return r, logger
 }
