d39cddb3a0138bd9c386f0d1c0a91b6a96304dcc
zcong1993 <zhangcong1992@gmail.com>
2020-12-31 00:15:48 +0800

    chore(example): add comments

--- _examples/gin/main.go
+++ _examples/gin/main.go
@@ -36,8 +36,11 @@ func main() {
 
 	app := &cobra.Command{
 		Run: func(cmd *cobra.Command, args []string) {
+			// 初始化日志
 			logger := log2.MustNewLogger(cmd)
+			// 初始化 gin
 			r := ginhelper.DefaultWithLogger(logger)
+
 			r.GET("/", func(c *gin.Context) {
 				requestTotal.WithLabelValues("/").Inc()
 				time.Sleep(2 * time.Second)
@@ -64,17 +67,21 @@ func main() {
 				return nil
 			}))
 
-			httpServer := exthttp.NewHttpServer(r, logger, exthttp.WithGracePeriod(time.Second*5), exthttp.WithListen(":8080"))
-
 			var g run.Group
+
+			// 监听退出信号
 			extrun.HandleSignal(&g)
 
+			httpServer := exthttp.NewHttpServer(r, logger, exthttp.WithGracePeriod(time.Second*5), exthttp.WithListen(":8080"))
+
+			// 真正的业务 http server
 			g.Add(func() error {
 				return httpServer.Start()
 			}, func(err error) {
 				httpServer.Shutdown(err)
 			})
 
+			// metrics 和 profiler 服务, debug 和监控
 			profileServer := exthttp.NewMuxServer(logger, exthttp.WithListen(":6060"), exthttp.WithServiceName("metrics/profiler"))
 			profileServer.RegisterProfiler()
 			profileServer.RegisterMetrics(nil)
@@ -86,6 +93,7 @@ func main() {
 		},
 	}
 
+	// 注册日志相关 flag
 	log2.Register(app)
 
 	if err := app.Execute(); err != nil {
