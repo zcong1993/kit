aa9e0d08ce47c22965421931b08f133335757306
zcong1993 <zhangcong1992@gmail.com>
2020-12-31 15:25:39 +0800

    feat(extflag): add path of content flag helper

--- _examples/logger/main.go
+++ _examples/logger/main.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"os"
 
+	"github.com/zcong1993/x/pkg/extflag"
+
 	"github.com/go-kit/kit/log/level"
 	"github.com/spf13/cobra"
 	"github.com/zcong1993/x/pkg/log"
@@ -16,11 +18,19 @@ func main() {
 	}
 
 	log.Register(rootCmd.PersistentFlags())
+	extflag.RegisterPathOrContent(rootCmd.PersistentFlags(), "config", "YAML file with tracing configuration.")
 
 	var testCmd = &cobra.Command{
 		Use:   "test",
 		Short: "Test sub command",
 		Run: func(cmd *cobra.Command, args []string) {
+			confContentYaml, err := extflag.LoadContent(cmd, "config", false)
+			if err != nil {
+				fmt.Println(err)
+				os.Exit(1)
+			} else {
+				fmt.Println(string(confContentYaml))
+			}
 			logger := log.MustNewLogger(cmd)
 			level.Info(logger).Log("msg", "test")
 		},
---
+++ pkg/extflag/extflag.go
@@ -0,0 +1,56 @@
+package extflag
+
+import (
+	"fmt"
+	"io/ioutil"
+
+	"github.com/spf13/cobra"
+
+	"github.com/pkg/errors"
+	"github.com/spf13/pflag"
+)
+
+func RegisterPathOrContent(flagSet *pflag.FlagSet, flagName string, help string) {
+	fileFlagName := fmt.Sprintf("%s-file", flagName)
+	contentFlagName := flagName
+
+	fileHelp := fmt.Sprintf("Path to %s", help)
+	flagSet.String(fileFlagName, "", fileHelp)
+
+	contentHelp := fmt.Sprintf("Alternative to '%s' flag (lower priority). Content of %s", fileFlagName, help)
+	flagSet.String(contentFlagName, "", contentHelp)
+}
+
+func LoadContent(cmd *cobra.Command, flagName string, required bool) ([]byte, error) {
+	contentFlagName := flagName
+	fileFlagName := fmt.Sprintf("%s-file", flagName)
+	path, err := cmd.Flags().GetString(fileFlagName)
+	if err != nil {
+		return nil, errors.Wrapf(err, "get flag %s error", fileFlagName)
+	}
+	contentStr, err := cmd.Flags().GetString(contentFlagName)
+	if err != nil {
+		return nil, errors.Wrapf(err, "get flag %s error", contentFlagName)
+	}
+
+	if len(path) > 0 && len(contentStr) > 0 {
+		return nil, errors.Errorf("both %s and %s flags set.", fileFlagName, contentFlagName)
+	}
+
+	var content []byte
+	if len(path) > 0 {
+		c, err := ioutil.ReadFile(path)
+		if err != nil {
+			return nil, errors.Wrapf(err, "loading YAML file %s for %s", path, fileFlagName)
+		}
+		content = c
+	} else {
+		content = []byte(contentStr)
+	}
+
+	if len(content) == 0 && required {
+		return nil, errors.Errorf("flag %s or %s is required for running this command and content cannot be empty.", fileFlagName, contentFlagName)
+	}
+
+	return content, nil
+}
