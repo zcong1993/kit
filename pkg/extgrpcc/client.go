7eed39425817781a49bbed46c270ebc54b17fc33
zcong1993 <zhangcong1992@gmail.com>
2021-01-19 01:12:37 +0800

    feat(tracing): add grpc client tracing

---
+++ pkg/extgrpcc/client.go
@@ -0,0 +1,33 @@
+package extgrpcc
+
+import (
+	"math"
+
+	grpc_middleware "github.com/grpc-ecosystem/go-grpc-middleware"
+	"github.com/opentracing/opentracing-go"
+	"github.com/zcong1993/x/pkg/tracing"
+	"google.golang.org/grpc"
+)
+
+// ClientGrpcOpts return return options with tracing and metrics
+func ClientGrpcOpts(tracer opentracing.Tracer, secure bool) []grpc.DialOption {
+	dialOpts := []grpc.DialOption{
+		grpc.WithDefaultCallOptions(grpc.MaxCallRecvMsgSize(math.MaxInt32)),
+		grpc.WithUnaryInterceptor(
+			grpc_middleware.ChainUnaryClient(
+				tracing.UnaryClientInterceptor(tracer),
+			),
+		),
+		grpc.WithStreamInterceptor(
+			grpc_middleware.ChainStreamClient(
+				tracing.StreamClientInterceptor(tracer),
+			),
+		),
+	}
+
+	if !secure {
+		dialOpts = append(dialOpts, grpc.WithInsecure())
+	}
+
+	return dialOpts
+}
