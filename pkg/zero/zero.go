2954f5ab7d3c54c1f5b1ee81a2b9d83a06b25023
zcong1993 <zhangcong1992@gmail.com>
2021-01-20 18:43:49 +0800

    chore(zero): disable go-zero log

--- pkg/zero/zero.go
+++ pkg/zero/zero.go
@@ -1,26 +1,33 @@
 package zero
 
 import (
+	"sync"
+
 	"github.com/tal-tech/go-zero/core/logx"
 	"github.com/tal-tech/go-zero/core/stat"
-	"github.com/tal-tech/go-zero/core/syncx"
 )
 
-var Metrics *stat.Metrics
+var (
+	Metrics *stat.Metrics
+	once    sync.Once
+)
 
+// SetupMetrics 创建一个全局 metrics.
 func SetupMetrics() {
-	syncx.Once(func() {
+	once.Do(func() {
 		setupZero()
 		Metrics = stat.NewMetrics("default")
 	})
 }
 
+// 处理外部依赖, go-zero 部分需要先初始化.
 func setupZero() {
 	stat.SetReporter(nil)
-	logx.MustSetup(logx.LogConf{
-		ServiceName:         "default",
-		Mode:                "console",
-		Level:               "info",
-		StackCooldownMillis: 100,
-	})
+	//logx.MustSetup(logx.LogConf{
+	//	ServiceName:         "default",
+	//	Mode:                "console",
+	//	Level:               "info",
+	//	StackCooldownMillis: 100,
+	//})
+	logx.Disable()
 }
